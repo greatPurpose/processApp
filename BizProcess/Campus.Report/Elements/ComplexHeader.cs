f5858dcf17455261b14f7ef528a4f78cf0ff7356
greatPurpose <happy.goodday@outlook.com>
2013-03-22 11:33:48 +0900

    ComplexHeader.cs added

---
+++ BizProcess/Campus.Report/Elements/ComplexHeader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Campus.Report.Base;
+
+namespace Campus.Report.Elements
+{
+    public class ComplexHeader : List<ComplexHeaderCell>, IElement
+    {
+        public Style Style { get; set; }
+    }
+}
