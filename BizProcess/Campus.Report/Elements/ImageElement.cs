919a688345a1a55b46357a2ba45ad3fe52dfeb4d
greatPurpose <happy.goodday@outlook.com>
2013-03-28 18:28:05 +0900

    ImageElement.cs added

---
+++ BizProcess/Campus.Report/Elements/ImageElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Campus.Report.Base;
+
+namespace Campus.Report.Elements
+{
+    public class ImageElement : IElement
+    {
+        public Image Image { get; set; }
+        public Uri Url { get; set; }
+
+        public ImageElement()
+        {
+        }
+
+        public ImageElement(string url)
+        {
+            Url = new Uri(url);
+        }
+
+        public ImageElement(System.Drawing.Image image)
+        {
+            Image = image;
+        }
+
+        public Style Style { get; private set; }
+    }
+}
