4f8db36b171427a6a832c346aabbbfec649d8df5
greatPurpose <happy.goodday@outlook.com>
2013-04-23 15:25:16 +0900

    SelectMemberController.cs added

---
+++ BizProcess/WebMvc/Areas/Controls/Controllers/SelectMemberController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMvc.Areas.Controls.Controllers
+{
+    public class SelectMemberController : MyController
+    {
+        //
+        // GET: /Controls/SelectMember/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetNames()
+        {
+            string values = Request.QueryString["values"];
+            return new BizProcess.Platform.Organize().GetNames(values);
+        }
+
+        public string GetNote()
+        {
+            string id = Request.QueryString["id"];
+            Guid gid;
+            if (id.IsNullOrEmpty())
+            {
+                return "";
+            }
+            BizProcess.Platform.Organize borg = new BizProcess.Platform.Organize();
+            BizProcess.Platform.Users buser = new BizProcess.Platform.Users();
+            if (id.StartsWith(BizProcess.Platform.Users.PREFIX))
+            {
+                Guid uid = buser.RemovePrefix1(id).ToGuid();
+                return string.Concat(borg.GetAllParentNames(buser.GetMainStation(uid)), " / ", buser.GetName(uid));
+            }
+            else if (id.StartsWith(BizProcess.Platform.WorkGroup.PREFIX))
+            {
+                return new BizProcess.Platform.WorkGroup().GetUsersNames(BizProcess.Platform.WorkGroup.RemovePrefix(id).ToGuid(), '、');
+            }
+            else if (id.IsGuid(out gid))
+            {
+                return borg.GetAllParentNames(gid);
+            }
+            return "";
+        }
+
+    }
+}
